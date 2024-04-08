861593dc9ee81b1097c930d0aa696304e70ac3d3
Wayne Lin <wclin@nuvoton.com>
2024-04-08 14:40:12 +0800

    Add M2354 Keil porting.

---
+++ board/numaker-hmi-m2354/main.c
@@ -0,0 +1,123 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @brief    M2354 Driver Sample Code
+ *
+ * @note
+ * Copyright (C) 2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+#include "lv_glue.h"
+
+static uint32_t get_systick(void)
+{
+    return sysGetTicks(0);
+}
+
+static void sys_init(void)
+{
+    SYS_UnlockReg();
+
+    /* LXT source from external LXT */
+    CLK_EnableModuleClock(RTC_MODULE);
+    RTC->LXTCTL &= ~(RTC_LXTCTL_LIRC32KEN_Msk | RTC_LXTCTL_C32KSEL_Msk);
+    CLK_DisableModuleClock(RTC_MODULE);
+
+    /* Enable clock source */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk | CLK_PWRCTL_LXTEN_Msk | CLK_PWRCTL_HXTEN_Msk | CLK_PWRCTL_HIRC48EN_Msk | CLK_PWRCTL_MIRCEN_Msk);
+
+    /* Waiting for clock source ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk | CLK_STATUS_LXTSTB_Msk | CLK_STATUS_HXTSTB_Msk | CLK_STATUS_HIRC48STB_Msk | CLK_STATUS_MIRCSTB_Msk);
+
+    /* Disable PLL first to avoid unstable when setting PLL */
+    CLK_DisablePLL();
+
+    /* Set PLL frequency */
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_96MHZ);
+
+    /* Waiting for PLL ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* If the defines do not exist in your project, please refer to the related clk.h in the Header folder appended to the tool package. */
+    /* Set HCLK clock */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL, CLK_CLKDIV0_HCLK(1));
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Enable UART0 module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL2_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA7MFP_Msk | SYS_GPA_MFPL_PA6MFP_Msk);
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA7MFP_UART0_TXD | SYS_GPA_MFPL_PA6MFP_UART0_RXD);
+
+    /* Enable EADC0 module clock/PIN */
+    CLK_EnableModuleClock(EADC_MODULE);
+    CLK_SetModuleClock(EADC_MODULE, MODULE_NoMsk, CLK_CLKDIV0_EADC(8));
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB11MFP_Msk | SYS_GPB_MFPH_PB10MFP_Msk | SYS_GPB_MFPH_PB9MFP_Msk | SYS_GPB_MFPH_PB8MFP_Msk);
+    SYS->GPB_MFPL &= ~(SYS_GPB_MFPL_PB7MFP_Msk | SYS_GPB_MFPL_PB6MFP_Msk);
+
+    /* Enable digital path on these EADC pins */
+    GPIO_ENABLE_DIGITAL_PATH(PB, BIT6 | BIT7 | BIT8 | BIT9 | BIT10 | BIT11);
+
+    /* Enable SPI1 module clock */
+    CLK_EnableModuleClock(SPI1_MODULE);
+    CLK_SetModuleClock(SPI1_MODULE, CLK_CLKSEL2_SPI1SEL_PCLK0, MODULE_NoMsk);
+    SYS->GPE_MFPL &= ~(SYS_GPE_MFPL_PE1MFP_Msk | SYS_GPE_MFPL_PE0MFP_Msk);
+    SYS->GPE_MFPL |= (SYS_GPE_MFPL_PE1MFP_SPI1_MISO | SYS_GPE_MFPL_PE0MFP_SPI1_MOSI);
+    SYS->GPH_MFPH &= ~(SYS_GPH_MFPH_PH9MFP_Msk | SYS_GPH_MFPH_PH8MFP_Msk);
+    SYS->GPH_MFPH |= (SYS_GPH_MFPH_PH9MFP_SPI1_SS | SYS_GPH_MFPH_PH8MFP_SPI1_CLK);
+
+    /* Enable SysTick module clock */
+    CLK_EnableSysTick(CLK_CLKSEL0_STCLKSEL_HCLK, 0);
+
+    /* Enable PDMA0/PDMA1 module clock */
+    CLK_EnableModuleClock(PDMA0_MODULE);
+    CLK_EnableModuleClock(PDMA1_MODULE);
+
+    UART_Open(UART0, 115200);
+
+    /* Vref connect to internal */
+    SYS_SetVRef(SYS_VREFCTL_VREF_3_0V);
+
+    systick_init();
+}
+
+#if LV_USE_LOG
+static void lv_nuvoton_log(const char *buf)
+{
+    printf("%s", buf);
+}
+#endif /* LV_USE_LOG */
+
+int main(void)
+{
+    sys_init();
+
+    lv_init();
+
+#if LV_USE_LOG
+    lv_log_register_print_cb(lv_nuvoton_log);
+#endif /* LV_USE_LOG */
+
+    lv_tick_set_cb(sysGetTicks);          /*Expression evaluating to current system time in ms*/
+    lv_delay_set_cb(sysDelay);
+
+    extern void lv_port_disp_init(void);
+    lv_port_disp_init();
+
+    extern void lv_port_indev_init(void);
+    lv_port_indev_init();
+
+    extern void ui_init(void);
+    ui_init();
+
+    while (1)
+    {
+        lv_task_handler();
+        // Put your code here  __start.
+        // Put your code here  __eend.
+    }
+
+    return 0;
+}
